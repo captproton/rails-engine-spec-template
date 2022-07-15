ca86d1fba464a4db765b1fd8004ea3705799c1f7
captproton <carl@wdwhub.net>
2022-07-15 12:10:05 -0700

    FIXED: the automatic creating of spec/dummy cd'ed into dummy and the script didn't go back to root

--- template.rb
+++ template.rb
@@ -1,6 +1,17 @@
 
 def git_commit(message)
   #FIX this has bugs
+  curdir = Dir.getwd
+  # keep line below for debugging later
+  # say "*$* Current directory is #{curdir}"
+
+  # get back to the gem's root directory
+  say "*$$ gem root directory is: #{destination_root}"
+  Dir.chdir destination_root
+  # keep lines below for debugging later
+  # curdir = Dir.getwd
+  # say "*$% Current directory is #{curdir}"
+
   git add: '.'
   git commit: "-m '#{message}' -q"
 end
@@ -16,7 +27,7 @@ say "Creating git repository..."
 
 GEMSPEC_FILE = File.join(destination_root, "#{name}.gemspec")
 RECIPE_PATH = File.join(File.dirname(rails_template), "recipes")
-RECIPES = %w{customize_meta dummy_app rspec guard developer_gems}
+RECIPES = %w{customize_meta dummy_app rspec guard developer_gems rails7_gems}
 
 RECIPES.each do |recipe|
   apply File.join(RECIPE_PATH, "#{recipe}.rb")
